add270ae932c1f995120500490958f4c008766f2
Brent Raines <bt.raines@gmail.com>
2014-03-10 17:09:21 -0400

    add effects to make learning look real

--- app/assets/javascripts/stylle.js
+++ app/assets/javascripts/stylle.js
@@ -1,14 +1,25 @@
 $(document).ready( function() {
 
 	$('.like').click(function() {
+		$this = $(this);
 		$('.improving').fadeIn(setTimeout(function() {
-			$('.improving').fadeOut();
+			$('.improving').fadeOut(function() {
+				$('.stream').fadeOut(setTimeout(function() {
+					$('.stream').fadeIn();
+				}, 50));
+			});
 		}, 2000));
 	});
 
 	$('.dislike').click(function() {
+		$this = $(this);
 		$('.improving').fadeIn(setTimeout(function() {
-			$('.improving').fadeOut();
+			$('.improving').fadeOut(function() {
+				$('.stream').fadeOut(setTimeout(function() {
+					$this.closest('.shoe').hide();
+					$('.stream').fadeIn();
+				}, 50));
+			});
 		}, 2000));
 	});
 
