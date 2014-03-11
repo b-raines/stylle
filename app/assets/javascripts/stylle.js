40d6a88a40798f9bcc90289b475299807d6dac75
Brent Raines <bt.raines@gmail.com>
2014-03-11 11:02:45 -0400

    add auto scroll on like

--- app/assets/javascripts/stylle.js
+++ app/assets/javascripts/stylle.js
@@ -2,10 +2,17 @@ $(document).ready( function() {
 
 	$('.like').click(function() {
 		$this = $(this);
+		var top = $(window).scrollTop();
+		var shoeHeight = $('.shoe').outerHeight();
 		$('.improving').fadeIn(setTimeout(function() {
 			$('.improving').fadeOut(function() {
-				$('.stream').fadeOut(setTimeout(function() {
-					$('.stream').fadeIn();
+				$('.stream').animate({
+					opacity: 0
+				}, setTimeout(function() {
+					$(window).scrollTop( top + shoeHeight);
+					$('.stream').animate({
+						opacity: 1
+					});
 				}, 50));
 			});
 		}, 2000));
